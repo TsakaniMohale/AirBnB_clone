f83da09acd4f5cb104eb7e69795793340075152b
Tsakani Mohale <tsakani Mohale>
2024-01-14 14:49:57 -0800

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -23,18 +23,18 @@ class BaseModel:
                 else:
                     setattr(self, key, value)
 
-       # models.storage.new(self)
+        models.storage.new(self)
 
     def save(self):
         """
-
+        Save method to update 'updated_at' and trigger storage save.
         """
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
         """
-
+        Convert instance attributes to a dictionary.
         """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
@@ -45,31 +45,6 @@ class BaseModel:
 
     def __str__(self):
         """
-
+        Convert instance to a string for printing.
         """
         class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
